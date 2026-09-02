82a3601be3b10b1972bcc3fe1dff842c3de34b59
amoremore123 <amoresalcmace@gmail.com>
2026-09-02 17:40:42 -0500

    Actualizar MinActivity para ver por emulador y verificar autor

--- Semana 03 /Laboratorio 03 /Semana03SalcedoM/app/src/main/java/com/fernandez/semana03salcedom/MainActivity.kt
+++ Semana 03 /Laboratorio 03 /Semana03SalcedoM/app/src/main/java/com/fernandez/semana03salcedom/MainActivity.kt
@@ -3,45 +3,24 @@ package com.fernandez.semana03salcedom
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.fernandez.semana03salcedom.ui.theme.Semana03SalcedoMTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Semana03SalcedoMTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    RegistroNotasScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Semana03SalcedoMTheme {
-        Greeting("Android")
     }
 }
