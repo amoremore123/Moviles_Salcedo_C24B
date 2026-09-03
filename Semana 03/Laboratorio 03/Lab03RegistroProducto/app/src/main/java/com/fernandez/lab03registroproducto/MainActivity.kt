3de64a596789ac0b9559e96ae301633015a6102c
amoremore123 <amoresalcmace@gmail.com>
2026-09-03 12:15:30 -0500

    Crea proyecto Lab03 en Semana03

---
+++ Semana 03/Laboratorio 03/Lab03RegistroProducto/app/src/main/java/com/fernandez/lab03registroproducto/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.fernandez.lab03registroproducto
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface { }
+            }
+        }
+    }
+}
